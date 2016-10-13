361f299509031eba5694f5ef6b8b7dd8ca5d8520
Alex H.Lamarche <alex.hlamarche@gmail.com>
2016-10-13 19:33:50 -0400

    Update MatchRangeSymbols.cpp

--- XtractK/XtractK/MatchRangeSymbols.cpp
+++ XtractK/XtractK/MatchRangeSymbols.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 
 MatchRangeSymbols::MatchRangeSymbols(symbol_t const first, symbol_t const last)
-	: mFirst{ first }, mLast{ last }
 {
+	mRange = new Range(first, last);
+}
+
+MatchRangeSymbols::~MatchrangeSymbols()
+{
+	delete mRange; // liberation de la memoire
 }
 
 bool MatchRangeSymbols::isMatching(symbol_t symbol) const
 {
-	return (symbol >= mFirst && symbol <= mLast) ? true : false;
+	return mRange->isInRange(symbol);
 }
 
 AbstractMatchSymbol* MatchRangeSymbols::clone() const
