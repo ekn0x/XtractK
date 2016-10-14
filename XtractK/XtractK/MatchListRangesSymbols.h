698b1b62156f43f33b66e65b9d7a98bddb5a8d41
Alex H.Lamarche <alex.hlamarche@gmail.com>
2016-10-13 20:49:23 -0400

    Update MatchListRangesSymbols.h

--- XtractK/XtractK/MatchListRangesSymbols.h
+++ XtractK/XtractK/MatchListRangesSymbols.h
@@ -14,41 +14,26 @@
 #ifndef MATCH_LIST_RANGES_SYMBOLS_H
 #define MATCH_LIST_RANGES_SYMBOLS_H
 
-#include "MatchRangeSymbols.h"
 #include <list>
 #include <initializer_list>
 
-class MatchRangeSymbols;
+class Range;
 
 using namespace std;
 
-class MatchListRangesSymbols : public AbstractMatchSymbol
+class MatchListRangeSymbols
 {
 public:
-	MatchListRangesSymbols() = delete;
-	//! Ce constructeur utilise une suite de symboles.
-	//!	Les symboles de debut et de fin ne peuvent être modifiés par la suite. 
-	MatchListRangesSymbols(symbol_t const first, symbol_t const last);
-	//! Constructeurs à l'aide de liste
-	//! List non initialisé
-	MatchListRangesSymbols(initializer_list<MatchRangeSymbols> const & rangeList);
-	//! Liste deja initialiser
-	MatchListRangesSymbols(list<MatchRangeSymbols> const & rangeList);
-	
-	//! Destructeur.
-	virtual ~MatchListRangesSymbols() = default;
+	MatchListRangeSymbols() = delete;
 
-	//! La fonction d'acceptation fait une comparaison du symbole et 
-	//!	retourne vrai si ce dernier est inclu dans la suite de symboles
-	//! de référence. La suite des symboles de référence est celle définie
-	//! lors de la définition de l'instance.
-	virtual bool isMatching(symbol_t symbol) const override;
+	MatchListRangeSymbols(initializer_list<Range> const & listRange);
 
-	// Retourne une nouvelle copie de l'instance courante.
-	virtual AbstractMatchSymbol* clone() const override;
+	~MatchListRangeSymbols() = default;
 
-protected:
-	list<MatchRangeSymbols> mRangeList;
+	bool isMatching(symbol_t const & symbol);
+
+private:
+	list<Range>  mListRange;
 };
 
 #endif
